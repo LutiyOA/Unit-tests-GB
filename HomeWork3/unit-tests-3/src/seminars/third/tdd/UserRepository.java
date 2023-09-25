772b68fd01e508457abba0b54cfe4c9c74471c42
Lutiy Oleg <lutiy.oleg@yandex.ru>
2023-09-26 00:26:27 +0700

    HomeWor3 complete

---
+++ HomeWork3/unit-tests-3/src/seminars/third/tdd/UserRepository.java
@@ -0,0 +1,44 @@
+package seminars.third.tdd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRepository {
+
+    // Тут можно хранить аутентифицированных пользователей
+    List<User> data = new ArrayList<>();
+
+    public void addUser(User user) {
+        //..
+        data.add(user);
+    }
+
+    public boolean findByName(String username) {
+        for (User user : data) {
+            if (user.name.equals(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public void unLoginAllUsersExceptAdmin() {
+        for (User user : data) {
+            if (!user.isAdmin)
+                user.isAuthenticate = false;
+        }
+    }
+
+    public boolean authenticateUser(String username, String password) {
+        if (findByName(username)) {
+            for (User user : data) {
+                if (user.name.equals(username)) {
+                    return user.authenticate(username, password);
+                }
+            }
+        }
+
+        return false;
+    }
+}
---
+++ HomeWork3/unit-tests-3/test/seminars/third/hw/testNumberInInterval.java
@@ -0,0 +1,27 @@
+package seminars.third.hw;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class testNumberInInterval {
+
+    private MainHW mainHW;
+
+    @BeforeEach
+    public void beforeEach() {
+        mainHW = new MainHW();
+    }
+
+    @Test
+    public void testInInterval() {
+        assertTrue(mainHW.numberInInterval(26));
+    }
+
+    @Test
+    public void testNotInInterval() {
+        assertFalse(mainHW.numberInInterval(0));
+    }
+}
